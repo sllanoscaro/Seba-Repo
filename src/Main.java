ee1b787ef0668602f2499a947338d4b86cbdd290
sebastianll <s.llanos03@ufromail.cl>
2023-08-24 10:14:46 -0400

    Commit actividades 1,2,3,4

--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,58 @@ public class Main {
         int[] mxn = leerDimensiones();
         validarDimensiones(mxn);
         int[][] matriz = crearMatriz(mxn);
+        mostrarMatriz(matriz);
+        llenarMatriz(matriz);
+        mostrarMatriz(matriz);
+
     }
+
     public static int[] leerDimensiones() {
         Scanner input = new Scanner(System.in);
         System.out.println("Ingresa las dimensiones de la matriz: ");
 
         int m = input.nextInt();
         int n = input.nextInt();
-        return(new int[]{m,n});
+        return (new int[]{m, n});
     }
+
     public static boolean validarDimensiones(int[] mxn) {
         int filas = mxn[0];
         int columnas = mxn[1];
 
         if (filas > 0 && columnas > 0) {
-            System.out.print("Entrada válida.");
+            System.out.print("Entrada válida. Matriz:\n");
             return true;
-        }
-        else {
-            System.out.print("Entrada inválida.");
+        } else {
+            System.out.print("Entrada inválida.\n");
             return false;
         }
     }
+
     public static int[][] crearMatriz(int[] mxn) {
         int filas = mxn[0];
         int columnas = mxn[1];
 
         return new int[filas][columnas];
     }
-    public static int[][] llenarMatriz(int[][] matriz) {
-        for (int fila = 0; fila < matriz[fila].length; fila++) {
-            for (int columna = 0; columna < matriz[fila].length; columna++) {
-                System.out.print(matriz[fila][columna] + " ");
+
+    public static void mostrarMatriz(int[][] matriz) {
+        for (int filas = 0; filas < matriz.length; filas++) {
+            for (int columnas = 0; columnas < matriz[filas].length; columnas++) {
+                System.out.print(matriz[filas][columnas] + " ");
             }
             System.out.println();
         }
     }
+    public static int[][] llenarMatriz(int[][] matriz) {
+        for (int filas = 0; filas < matriz.length; filas++) {
+            for (int columnas = 0; columnas < matriz[filas].length; columnas++) {
+                matriz[filas][columnas] = (int) Math.floor(Math.random() * 10);
+            }
+            System.out.println();
+        }
+        return matriz;
+    }
 }
+
+
