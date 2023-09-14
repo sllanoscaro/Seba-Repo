009904e99a49be6cef445a66125b1638450c99cf
sebastianll <s.llanos03@ufromail.cl>
2023-09-13 22:21:14 -0300

    Segunda versión Calculadora vCientífica.

--- Código Taller N°1/src/CalculadoraCientifica.java
+++ Código Taller N°1/src/CalculadoraCientifica.java
@@ -1,49 +1,57 @@
-import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class CalculadoraCientifica {
     public static void main(String[] args) {
-        //sumaNum(entradaNum());
+        sumaNum(entradaNum());
         //restaNum(entradaNum());
         //multiplicarNum(entradaNum());
         //dividirNum(entradaNum());
         //solucionesCuadratica(entradaCoeficientes());
         //resolverSistema(creacionSistema());
 
-        double[] punto1 = coordenadasPunto();
-        double[] punto2 = coordenadasPunto();
-        calcularEcRecta(punto1, punto2);
+        //double[] punto1 = coordenadasPunto();
+        //double[] punto2 = coordenadasPunto();
+        //calcularEcRecta(punto1, punto2);
     }
 
     public static double[] entradaNum() {
         Scanner entrada = new Scanner(System.in);
         double[] array = new double[2];
         System.out.println("Ingrese los números a operar: ");
-        array[0] = entrada.nextDouble();
-        array[1] = entrada.nextDouble();
-        return array;
+        try {
+            array[0] = entrada.nextDouble();
+            array[1] = entrada.nextDouble();
+            return array;
+        } catch (InputMismatchException e) {
+            System.out.print("Se han ingresado parámetros incorrectos, intente de nuevo.\n");
+            throw e;
+        }
     }
 
-    public static void sumaNum(double[] array) {
+    public static double sumaNum(double[] array) {
         double suma = array[0] + array[1];
         System.out.print("La suma de ambos números es: " + suma);
+        return suma;
     }
 
-    public static void restaNum(double[] array) {
+    public static double restaNum(double[] array) {
         double resta = array[0] - array[1];
         System.out.print("La resta de ambos números es: " + resta);
+        return resta;
     }
 
-    public static void multiplicarNum(double[] array) {
+    public static double multiplicarNum(double[] array) {
         double multiplicacion = array[0] * array[1];
         System.out.print("La multiplicación de ambos números es: " + multiplicacion);
+        return multiplicacion;
     }
 
-    public static void dividirNum(double[] array) {
+    public static double dividirNum(double[] array) {
         while (true) {
             if (array[1] != 0) {
                 double division = array[0] / array[1];
                 System.out.print("La división de ambos números es: " + division);
-                break;
+                return division;
             } else {
                 System.out.println("El divisor debe ser distinto de 0.");
                 array = entradaNum();
@@ -55,13 +63,18 @@ public class CalculadoraCientifica {
         Scanner entrada = new Scanner(System.in);
         double[] array = new double[3];
         System.out.println("Ingrese los coeficientes de la ecuación: ");
-        array[0] = entrada.nextDouble();
-        array[1] = entrada.nextDouble();
-        array[2] = entrada.nextDouble();
-        return array;
+        try {
+            array[0] = entrada.nextDouble();
+            array[1] = entrada.nextDouble();
+            array[2] = entrada.nextDouble();
+            return array;
+        } catch (InputMismatchException e) {
+            System.out.print("Se han ingresado parámetros incorrectos, intente de nuevo.\n");
+            throw e;
+        }
     }
 
-    public static void solucionesCuadratica(double[] coeficientes) {
+    public static double solucionesCuadratica(double[] coeficientes) {
         double discriminante = (coeficientes[1] * coeficientes[1]) - 4 * (coeficientes[0] * coeficientes[2]);
 
         if (discriminante < 0) {
@@ -76,56 +89,67 @@ public class CalculadoraCientifica {
             System.out.println("X1 = " + resultado1);
             System.out.print("X2 = " + resultado2);
         }
+        return discriminante;
     }
 
-    public static void perimetroRectangulo(double[] array) {
+    public static double perimetroRectangulo(double[] array) {
         double resultado = 2 * (array[0] + array[1]);
         System.out.print("El perímetro del rectángulo es: " + resultado);
+        return resultado;
     }
 
-    public static void areaRectangulo(double[] array) {
+    public static double areaRectangulo(double[] array) {
         double resultado = array[0] * array[1];
         System.out.print("El área del rectángulo es: " + resultado);
+        return resultado;
     }
 
-    public static void perimetroCirculo(double radio) {
+    public static double perimetroCirculo(double radio) {
         double resultado = 2 * Math.PI * radio;
         System.out.print("El perímetro del circulo es: " + resultado);
+        return resultado;
     }
 
-    public static void areaCirculo(double radio) {
-        double resultado = Math.PI * Math.PI * radio;
+    public static double areaCirculo(double radio) {
+        double resultado = Math.PI * radio * radio;
         System.out.print("El área del circulo es: " + resultado);
+        return resultado;
     }
 
-    public static void areaEsfera(double radio) {
+    public static double areaEsfera(double radio) {
         double resultado = 4 * Math.PI * radio * radio;
         System.out.print("El área de la esfera es: " + resultado);
+        return resultado;
     }
 
-    public static void volumenEsfera(double radio) {
+    public static double volumenEsfera(double radio) {
         double resultado = ((double) 4 / 3) * Math.PI * radio * radio * radio;
         System.out.print("El volumen de la esfera es: " + resultado);
+        return resultado;
     }
 
-    public static void areaCubo(double largo) {
+    public static double areaCubo(double largo) {
         double resultado = 6 * largo * largo;
         System.out.print("El área del cubo es: " + resultado);
+        return resultado;
     }
 
-    public static void volumenCubo(double largo) {
+    public static double volumenCubo(double largo) {
         double resultado = largo * largo * largo;
         System.out.print("El volumen del cubo es: " + resultado);
+        return resultado;
     }
 
-    public static void areaCono(double[] array) {
+    public static double areaCono(double[] array) {
         double resultado = Math.PI * array[0] * array[0] + Math.PI * array[0] * Math.sqrt(array[1] * array[1] + array[0] * array[0]);
         System.out.print("El área del cono es: " + resultado);
+        return resultado;
     }
 
-    public static void volumenCono(double[] array) {
+    public static double volumenCono(double[] array) {
         double resultado = ((double) 1 / 3) * Math.PI * array[0] * array[0] * array[1];
         System.out.print("El volumen del cono es: " + resultado);
+        return resultado;
     }
 
     public static int[][] creacionSistema() {
@@ -134,14 +158,19 @@ public class CalculadoraCientifica {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print("Ingrese el elemento en la posición [" + i + "][" + j + "]: ");
-                matriz[i][j] = entrada.nextInt();
+                try {
+                    matriz[i][j] = entrada.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.print("Se han ingresado parámetros incorrectos, intente de nuevo.\n");
+                    throw e;
+                }
             }
         }
 
         return matriz;
     }
 
-    public static void resolverSistema(int[][] matriz) {
+    public static double resolverSistema(int[][] matriz) {
         double determinante = matriz[0][0] * matriz[1][1] - matriz[1][0] * matriz[0][1];
 
         if (determinante == 0) {
@@ -153,20 +182,27 @@ public class CalculadoraCientifica {
             System.out.println("X = " + solucionX);
             System.out.print("Y = " + solucionY);
         }
+        return determinante;
     }
 
     public static double[] coordenadasPunto() {
         Scanner entrada = new Scanner(System.in);
         double[] coordenada = new double[2];
         System.out.println("Ingrese el par de coordenadas del punto: ");
-        coordenada[0] = entrada.nextDouble();
-        coordenada[1] = entrada.nextDouble();
-        return coordenada;
+        try {
+            coordenada[0] = entrada.nextDouble();
+            coordenada[1] = entrada.nextDouble();
+            return coordenada;
+        } catch (InputMismatchException e) {
+            System.out.print("Se han ingresado parámetros incorrectos, intente de nuevo.\n");
+            throw e;
+        }
     }
-    public static void calcularEcRecta(double[] punto1, double[] punto2) {
+    public static double calcularEcRecta(double[] punto1, double[] punto2) {
         double pendiente = (punto2[1] - punto1[1]) / (punto2[0] - punto1[0]);
         double termino_independiente = punto1[1] - pendiente * punto1[0];
         System.out.print("La ecuación de la recta es: y = " + pendiente + "x + " + termino_independiente);
+        return pendiente;
     }
-}
 
+}
--- Código Taller N°1/src/CalculadoraCientificaTest.java
+++ Código Taller N°1/src/CalculadoraCientificaTest.java
@@ -16,69 +16,121 @@ class CalculadoraCientificaTest {
 
     @Test
     void sumaNum() {
+        double[] array = {0, 2.0};
+        double resultado = CalculadoraCientifica.sumaNum(array);
+        assertEquals(2.0, resultado, 0.001);
     }
 
     @Test
     void restaNum() {
+        double[] array = {-7.0, -8.6};
+        double resultado = CalculadoraCientifica.restaNum(array);
+        assertEquals(1.6, resultado, 0.001);
     }
 
     @Test
     void multiplicarNum() {
+        double[] array = {10, -8.6};
+        double resultado = CalculadoraCientifica.multiplicarNum(array);
+        assertEquals(-86.0, resultado, 0.001);
     }
 
     @Test
     void dividirNum() {
+        double[] array = {17, 9};
+        double resultado = CalculadoraCientifica.dividirNum(array);
+        assertEquals(1.888, resultado, 0.001);
     }
 
     @Test
     void solucionesCuadratica() {
+        double[] coeficientes = {1, 4 , 5};
+        double discriminante = CalculadoraCientifica.solucionesCuadratica(coeficientes);
+        assertEquals(-4, discriminante, 0.001);
     }
 
     @Test
     void perimetroRectangulo() {
+        double[] lados = {4, 4};
+        double resultado = CalculadoraCientifica.perimetroRectangulo(lados);
+        assertEquals(16, resultado, 0.001);
     }
 
     @Test
     void areaRectangulo() {
+        double[] lados = {4, 5};
+        double resultado = CalculadoraCientifica.areaRectangulo(lados);
+        assertEquals(20, resultado, 0.001);
     }
 
     @Test
     void perimetroCirculo() {
+        double radio = 4;
+        double resultado = CalculadoraCientifica.perimetroCirculo(radio);
+        assertEquals(25.132, resultado, 0.001);
     }
 
     @Test
     void areaCirculo() {
+        double radio = 7;
+        double resultado = CalculadoraCientifica.areaCirculo(radio);
+        assertEquals(153.938, resultado, 0.001);
     }
 
     @Test
     void areaEsfera() {
+        double radio = 5;
+        double resultado = CalculadoraCientifica.areaEsfera(radio);
+        assertEquals(314.159, resultado, 0.001);
     }
 
     @Test
     void volumenEsfera() {
+        double radio = 2;
+        double resultado = CalculadoraCientifica.volumenEsfera(radio);
+        assertEquals(33.510, resultado, 0.001);
     }
 
     @Test
     void areaCubo() {
+        double largo = 5;
+        double resultado = CalculadoraCientifica.areaCubo(largo);
+        assertEquals(150, resultado, 0.001);
     }
 
     @Test
     void volumenCubo() {
+        double largo = 2;
+        double resultado = CalculadoraCientifica.volumenCubo(largo);
+        assertEquals(8, resultado, 0.001);
     }
 
     @Test
     void areaCono() {
+        double[] alturaradio = {10, 5};
+        double resultado = CalculadoraCientifica.areaCono(alturaradio);
+        assertEquals(665.400, resultado, 0.001);
     }
 
     @Test
     void volumenCono() {
+        double[] alturaradio = {10, 5};
+        double resultado = CalculadoraCientifica.volumenCono(alturaradio);
+        assertEquals(523.598, resultado, 0.001);
     }
 
     @Test
     void resolverSistema() {
+        int[][] matriz = {{1,2,3},{4,5,6}};
+        double resultado = CalculadoraCientifica.resolverSistema(matriz);
+        assertEquals(-3, resultado, 0.001);
     }
 
     @Test
     void calcularEcRecta() {
+        double[] punto1 = {2,2};
+        double[] punto2 = {1,1};
+        double resultado = CalculadoraCientifica.calcularEcRecta(punto1,punto2);
+        assertEquals(1, resultado, 0.001);
     }
 }
