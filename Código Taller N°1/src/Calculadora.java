b7433741d9e38433c0f5403a7ffacfeff5447567
sebastianll <s.llanos03@ufromail.cl>
2023-10-11 20:19:04 -0300

    Commit actividad de análisis de codigo y dependencias.

---
+++ Código Taller N°1/src/Calculadora.java
@@ -0,0 +1,47 @@
+public class Calculadora {
+
+	private int n1;
+	private int n2;
+
+	public Calculadora() {
+		// TODO - implement Calculadora.Calculadora
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param num1
+	 * @param num2
+	 */
+	public Calculadora(int num1, int num2) {
+		// TODO - implement Calculadora.Calculadora
+		throw new UnsupportedOperationException();
+	}
+
+	public int sumar() {
+		// TODO - implement Calculadora.sumar
+		throw new UnsupportedOperationException();
+	}
+
+	public int multilplicar() {
+		// TODO - implement Calculadora.multilplicar
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param n1
+	 */
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+
+	/**
+	 * 
+	 * @param n2
+	 */
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+
+}
---
+++ Código Taller N°1/src/CarroCompra.java
@@ -0,0 +1,30 @@
+public class CarroCompra {
+
+	private int[][] productos;
+
+	public CarroCompra() {
+		// TODO - implement CarroCompra.CarroCompra
+		throw new UnsupportedOperationException();
+	}
+
+	private int calcularTotal() {
+		// TODO - implement CarroCompra.calcularTotal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cant
+	 * @param precio
+	 */
+	private int subTotal(int cant, int precio) {
+		// TODO - implement CarroCompra.subTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		// TODO - implement CarroCompra.mostrarTotal
+		throw new UnsupportedOperationException();
+	}
+
+}
